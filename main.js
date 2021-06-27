4940ddeca9bdf9eb911c4754bb9f568f93e10ccc
Devpsud <81077877+Devpsud@users.noreply.github.com>
2021-06-27 11:03:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,88 @@
+song = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+scoreLeftWrist = 0;
+scoreRightWrist = 0;
+
+function setup() {
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function draw() {
+    image(video, 0, 0, 600, 500);
+
+    fill("#FF0000");
+    stroke("#FF0000");
+
+if (scoreLeftWrist > 0.2) {
+    circle(leftWristX,leftWristY,20);
+    wrist_leftWristY = Number(leftWristY);
+    remove_decimal = floor(wrist_leftWristY);
+     volume = remove_decimal/500;
+     document.getElementById("volume").innerHTML = "volume = "+volume;
+     song.setVolume(volume);
+}
+if (scoreRightWrist >0.2) {
+    circle(rightWristX,rightWristY,20);
+    wrist_rightWristY = Number(rightWristY);
+    remove_decimal = floor(wrist_rightWristY);
+console.log("rate = "+remove_decimal);
+    if (rightWristY >0 && rightWristY <= 100) {
+        document.getElementById("speed").innerHTML = "speed = 0.5x";
+        song.rate(0.5);
+    }
+    if (rightWristY >100 && rightWristY <= 200) {
+        document.getElementById("speed").innerHTML = "speed = 1x";
+        song.rate(1);
+    }
+    if (rightWristY >200 && rightWristY <= 300) {
+        document.getElementById("speed").innerHTML = "speed = 1.5x";
+        song.rate(1.5);
+    }
+    if (rightWristY >300 && rightWristY <= 400) {
+        document.getElementById("speed").innerHTML = "speed = 2x";
+        song.rate(2);
+    }
+    if (rightWristY >400 && rightWristY <= 500) {
+        document.getElementById("speed").innerHTML = "speed = 2.5x";
+        song.rate(2.5);
+    }
+}
+}
+
+function preload() {
+    song = loadSound("music.mp3");
+}
+
+function play() {
+    song.play();
+    song.setVolume(1);
+    song.rate(1);
+}
+
+function modelLoaded() {
+    console.log("PoseNet Model Is Loaded");
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        scoreRightWrist = results[0].pose.keypoints[10].score;
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log(" Left Wrist X = " + leftWristX + " left Wrist Y = " + leftWristY);
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log(" Right Wrist X = " + rightWristX + " right Wrist Y = " + rightWristY);
+    }
+}
